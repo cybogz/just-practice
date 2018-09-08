523dba2d42742de6162d74db47d3c04396c4814a
Cyrus Bogzaran <cyruscbcbcb93@hotmail.com>
2018-09-07 22:58:46 -0700

    Finished dayAndTime function

--- javascript/addition.js
+++ javascript/addition.js
@@ -1,18 +1,28 @@
-var today = new Date()
-var day = today.getUTCDay();
+function dayAndTime()
+{
+	var today = new Date()
+	var day = today.getUTCDay();
 
-var nameOfDay = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+	var nameOfDay = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-var thisDate = nameOfDay[day];
+	var thisDate = nameOfDay[day];
 
-console.log("Today is: " + thisDate + ".");
+	//console.log(+ ".");
 
-var hour = today.getHours();
-var minutes = today.getMinutes();
-var seconds = today.getSeconds();
+	var hour = today.getHours();
+	var minutes = today.getMinutes();
+	var seconds = today.getSeconds();
 
+	if(hour > 12)
+	{
+		var pm = "Today is " + thisDate + ": " + (hour - 12) + ":" + minutes + ":" + seconds + " PM";
+		return pm;
+	}
+	else
+	{
+		var am = "Today is " + thisDate + ": " + hour + ":" + minutes + ":" + seconds + " AM";
+		return am;
+	}
+}
 
-
-
-
-console.log(hour + " " + minutes + " " + seconds);
+console.log(dayAndTime());
